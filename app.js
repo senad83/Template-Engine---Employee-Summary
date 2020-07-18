e867e141afd1b8bae5e177d79dc4a9fc56925e2c
Senad <senadutah@gmail.com>
2020-07-17 18:29:36 -0600

    cleaned code

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// creating empty array for storing input from a user, used syntax from Employee.js file 
-// and read a little on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/constructor,
-// also went to bunch a projects on gitHub and analize code
-
-
-   
         var entry = [];
         var currentEntry = { name: "", id: "", email: ""};
 
@@ -56,7 +50,6 @@ function addManager() {
     inquirer.prompt([
         { type: "input", message: "what is your office number?", name: "officeNumber"}
     ]).then (response => {
-        // i saw on solved solution that they're using response for specified things, why?
         const manager = new Manager(currentEntry.name, currentEntry.id, currentEntry.email, response.officeNumber);
         entry.push(manager);
         askUserToContinue();
@@ -100,21 +93,11 @@ function addEmployee(){
         }
 
     ]).then(response => {
-        console.log(response.empltype, 'here part 2')
-        console.log(response.choices, "here")
-        // with this I should save current entries of id, name and email,
-        // that's what other people did on but I cannot make it work
         
         currentEntry.name = response.name;
         currentEntry.id = response.id;
         currentEntry.email = response.email
         console.log (response)
-
-        // need to fix this is not creating array
-        
-        // tried to pull information from here for next step but adding manager doesn't work
-        // or console.log either
-        // adedd response.empltype with help of assistant on bcs
         
         if (response.empltype === "Manager") {
            addManager();
@@ -126,32 +109,5 @@ function addEmployee(){
        });
     };
 
-// this part i got from ask bootcamp spot
-// ask Chris to explain this part
-// const array = new EmptyArray();
-// array.addEmployee();
-
 addEmployee();
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ````
